954bd0b3f6b24e3f68592798e716d35db26e9bb2
Andy Albaugh <andypsu5155@gmail.com>
2024-01-21 23:30:58 -0500

    Got a working display set up at the very least.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,101 @@
 "use client";
-import Image from "next/image";
-import React from "react";
-import { epg as epgData, channels as channelData } from "@/test";
 
-import { useEpg, Epg, Layout } from "planby";
+import React from "react";
+//import { epg as epgData, channels as channelData } from "@/test2";
+import { createChannelsEpgs } from "@/data/test-data";
+
+import {
+  useEpg,
+  Epg,
+  Layout,
+  ChannelBox,
+  ChannelLogo,
+  Channel,
+  ProgramBox,
+  ProgramContent,
+  ProgramFlex,
+  ProgramStack,
+  ProgramTitle,
+  ProgramText,
+  ProgramImage,
+  useProgram,
+  Program,
+  ProgramItem,
+} from "planby";
+
+interface ChannelItemProps {
+  channel: Channel;
+}
+
+const Item = ({ program, ...rest }: ProgramItem) => {
+  const { styles, formatTime, isLive, isMinWidth } = useProgram({
+    program,
+    ...rest,
+  });
+
+  const { data } = program;
+  const { image, title, since, till } = data;
+
+  const sinceTime = formatTime(since);
+  const tillTime = formatTime(till);
+
+  if (title !== "Lunch") {
+    return (
+      <ProgramBox width={styles.width} style={styles.position}>
+        <ProgramContent width={styles.width} isLive={isLive}>
+          <ProgramFlex>
+            {isLive && isMinWidth && <ProgramImage src={image} alt="Preview" />}
+            <ProgramStack>
+              <ProgramTitle>{title}</ProgramTitle>
+              <ProgramText>
+                {sinceTime} - {tillTime}
+              </ProgramText>
+            </ProgramStack>
+          </ProgramFlex>
+        </ProgramContent>
+      </ProgramBox>
+    );
+  } else {
+    return (
+      <ProgramBox
+        width={styles.width}
+        style={styles.position}
+        className="!bg-red-400 rounded-xl"
+      >
+        <ProgramContent width={styles.width} isLive={isLive}>
+          <ProgramFlex>
+            {isLive && isMinWidth && <ProgramImage src={image} alt="Preview" />}
+            <ProgramStack>
+              <ProgramTitle>{title}</ProgramTitle>
+              <ProgramText>
+                {sinceTime} - {tillTime}
+              </ProgramText>
+            </ProgramStack>
+          </ProgramFlex>
+        </ProgramContent>
+      </ProgramBox>
+    );
+  }
+};
+
+const ChannelItem = ({ channel }: ChannelItemProps) => {
+  const { position, logo } = channel;
+
+  return (
+    <ChannelBox {...position}>
+      <ChannelLogo
+        onClick={() => console.log("channel", channel)}
+        src={logo}
+        alt="Logo"
+        className="p-3"
+      />
+      <p className="pr-5">{channel.title}</p>
+    </ChannelBox>
+  );
+};
 
 export default function Home() {
+  const { epgs: epgData, channels: channelData } = createChannelsEpgs();
   const channels = React.useMemo(() => channelData, []);
 
   const epg = React.useMemo(() => epgData, []);
@@ -19,14 +109,23 @@ export default function Home() {
   } = useEpg({
     epg,
     channels,
-    startDate: "2022-10-18T00:00:00", // or 2022-02-02T00:00:00
+    startDate: "2024-01-17T15:00:00", // or 2022-02-02T00:00:00
+    endDate: "2024-01-18T08:00:00",
   });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div style={{ height: "600px", width: "1200px" }}>
+      <div className="w-[1920px]">
         <Epg {...getEpgProps()}>
-          <Layout {...getLayoutProps()} />
+          <Layout
+            {...getLayoutProps()}
+            renderChannel={({ channel }) => (
+              <ChannelItem key={channel.uuid} channel={channel} />
+            )}
+            renderProgram={({ program, ...rest }) => (
+              <Item key={program.data.id} program={program} {...rest} />
+            )}
+          />
         </Epg>
       </div>
     </main>
---
+++ test2.ts
@@ -0,0 +1,22 @@
+export const epg = [
+  {
+    id: "6f3caa7f-5b11-4edb-998e-80d4baa03373",
+    description: "Time for lunch",
+    title: "Lunch",
+    isYesterday: true,
+    since: "2022-10-17T23:50:00",
+    till: "2022-10-18T00:55:00",
+    channelUuid: "16fdfefe-e466-4090-bc1a-57c43937f826",
+    image:
+      "https://www.themoviedb.org/t/p/w1066_and_h600_bestv2/sjx6zjQI2dLGtEL0HGWsnq6UyLU.jpg",
+  },
+];
+
+export const channels = [
+  {
+    uuid: "16fdfefe-e466-4090-bc1a-57c43937f826",
+    type: "channel",
+    title: "Andy",
+    logo: "https://raw.githubusercontent.com/karolkozer/planby-demo-resources/master/resources/channel-logos/png/r-channel.png",
+  },
+];
